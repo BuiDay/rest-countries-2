596814864e1f3ebb07a52df88e60aca97c4d4a44
BuiDay <buivanduynhat@gmail.com>
2022-08-26 14:18:32 +0700

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="title"> Where are you from?</h1>
+        </div>
+    );
+};
+
+export default Header;
